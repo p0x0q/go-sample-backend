e1b2a62c58d1d3aecfee6f112cd87f95e0e31824
p0x0q <info.allegation@gmail.com>
2022-07-10 06:05:50 +0900

    test main.go

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+
+	// docsのディレクトリを指定
+	_ "aipco-backend/docs" // ←追記
+
+	ginSwagger "github.com/swaggo/gin-swagger" // ←追記
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "レスポンスだよ",
-		})
-	})
+
+	// 下記を追記することで`http://localhost:8080/swagger/index.html`を叩くことでswagger uiを開くことができる
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	r.Run()
 }
