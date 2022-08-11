e42ad340bc0a959c343c6675023fdf7bbf9fa14a
p0x0q <info.allegation@gmail.com>
2022-08-12 05:15:53 +0900

    generate files

--- go/api_pets.go
+++ go/api_pets.go
@@ -10,12 +10,70 @@
 package openapi
 
 import (
+	"encoding/json"
 	"net/http"
+	"strings"
 
-	"github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
 )
 
+// PetsApiController binds http requests to an api service and writes the service results to the http response
+type PetsApiController struct {
+	service PetsApiServicer
+	errorHandler ErrorHandler
+}
+
+// PetsApiOption for how the controller is set up.
+type PetsApiOption func(*PetsApiController)
+
+// WithPetsApiErrorHandler inject ErrorHandler into controller
+func WithPetsApiErrorHandler(h ErrorHandler) PetsApiOption {
+	return func(c *PetsApiController) {
+		c.errorHandler = h
+	}
+}
+
+// NewPetsApiController creates a default api controller
+func NewPetsApiController(s PetsApiServicer, opts ...PetsApiOption) Router {
+	controller := &PetsApiController{
+		service:      s,
+		errorHandler: DefaultErrorHandler,
+	}
+
+	for _, opt := range opts {
+		opt(controller)
+	}
+
+	return controller
+}
+
+// Routes returns all the api routes for the PetsApiController
+func (c *PetsApiController) Routes() Routes {
+	return Routes{ 
+		{
+			"ListPets",
+			strings.ToUpper("Get"),
+			"/v1/pets",
+			c.ListPets,
+		},
+	}
+}
+
 // ListPets - List all pets
-func ListPets(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{})
+func (c *PetsApiController) ListPets(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	limitParam, err := parseInt32Parameter(query.Get("limit"), false)
+	if err != nil {
+		c.errorHandler(w, r, &ParsingError{Err: err}, nil)
+		return
+	}
+	result, err := c.service.ListPets(r.Context(), limitParam)
+	// If an error occurred, encode the error with the status code
+	if err != nil {
+		c.errorHandler(w, r, err, &result)
+		return
+	}
+	// If no error, encode the body and the result code
+	EncodeJSONResponse(result.Body, &result.Code, w)
+
 }
--- go/model_pet.go
+++ go/model_pet.go
@@ -17,3 +17,30 @@ type Pet struct {
 
 	Tag string `json:"tag,omitempty"`
 }
+
+// AssertPetRequired checks if the required fields are not zero-ed
+func AssertPetRequired(obj Pet) error {
+	elements := map[string]interface{}{
+		"id": obj.Id,
+		"name": obj.Name,
+	}
+	for name, el := range elements {
+		if isZero := IsZeroValue(el); isZero {
+			return &RequiredError{Field: name}
+		}
+	}
+
+	return nil
+}
+
+// AssertRecursePetRequired recursively checks if required fields are not zero-ed in a nested slice.
+// Accepts only nested slice of Pet (e.g. [][]Pet), otherwise ErrTypeAssertionError is thrown.
+func AssertRecursePetRequired(objSlice interface{}) error {
+	return AssertRecurseInterfaceRequired(objSlice, func(obj interface{}) error {
+		aPet, ok := obj.(Pet)
+		if !ok {
+			return ErrTypeAssertionError
+		}
+		return AssertPetRequired(aPet)
+	})
+}
